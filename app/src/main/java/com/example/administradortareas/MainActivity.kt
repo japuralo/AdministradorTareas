b4a5e4b06a05840286f880ec6b7cbf31ce1aa80f
Javier Alejandro Puras Lombillo <javijandro99@gmail.com>
2024-02-18 23:34:01 +0100

    Initial commit

---
+++ app/src/main/java/com/example/administradortareas/MainActivity.kt
@@ -0,0 +1,79 @@
+package com.example.administradortareas
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.administradortareas.ui.theme.AdministradorTareasTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            AdministradorTareasTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Tarea()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Tarea(modifier: Modifier = Modifier)
+{
+    val image = painterResource(R.drawable.ic_task_completed)
+    Column(
+        verticalArrangement = Arrangement.Center
+    )
+    {
+        Image(
+            painter = image,
+            contentDescription = null,
+            modifier = Modifier
+                .align(alignment = Alignment.CenterHorizontally)
+        )
+        Text(
+            text = "All tasks completed",
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier
+                .padding(top = 24.dp, bottom = 8.dp)
+                .align(alignment = Alignment.CenterHorizontally)
+        )
+        Text(
+            text = "Nice work!",
+            fontSize = 16.sp,
+            modifier = Modifier
+                .align(alignment = Alignment.CenterHorizontally)
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    AdministradorTareasTheme {
+        Tarea()
+    }
+}
